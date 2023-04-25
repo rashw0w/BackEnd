2c3f0ad817e52ed304199e81ef222b97ec424885
rashw0w <naacho991@gmail.com>
2023-04-25 09:19:40 -0300

    moved backend

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM amazoncorretto:11-alpine-jdk
+MAINTAINER RASH
+COPY target/rash-0.0.1-SNAPSHOT.jar rash-app.jar
+ENTRYPOINT ["java","-jar","/rash-app.jar"]
---
+++ src/main/java/com/portfolio/rash/RashApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.rash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RashApplication.class, args);
+	}
+
+}
