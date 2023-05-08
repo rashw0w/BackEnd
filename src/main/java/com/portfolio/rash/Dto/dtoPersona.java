7e0e087ef6d659ca7dc90f58a26903cfce23c2fd
rashw0w <naacho991@gmail.com>
2023-05-08 11:53:41 -0300

    fix

--- src/main/java/com/portfolio/rash/Dto/dtoPersona.java
+++ src/main/java/com/portfolio/rash/Dto/dtoPersona.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.portfolio.rash.Dto;
 
 import javax.validation.constraints.NotBlank;
 
-/**
- *
- * @author Usuario
- */
+
 public class dtoPersona {
     @NotBlank
     private String nombre;
