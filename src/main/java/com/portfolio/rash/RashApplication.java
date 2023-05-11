ae4917bfea40cdd05aa6844437dd438ea98dd55c
rashw0w <naacho991@gmail.com>
2023-05-11 11:13:37 -0300

    cors

--- src/main/java/com/portfolio/rash/RashApplication.java
+++ src/main/java/com/portfolio/rash/RashApplication.java
@@ -3,6 +3,9 @@ package com.portfolio.rash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -10,5 +13,16 @@ public class RashApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RashApplication.class, args);
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://rashw0wportfolio.web.app/");
+            }
+        };
     }
 }
